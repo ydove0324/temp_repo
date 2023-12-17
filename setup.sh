9ec7c90005f2bc6c6334908d7849bb2d7e77af07
ydove0324 <111854489+ydove0324@users.noreply.github.com>
2023-12-17 23:49:42 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
-wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/gcc-3.4-base_3.4.6-6ubuntu3_i386.deb
-dpkg -i gcc-3.4-base_3.4.6-6ubuntu3_i386.deb
-wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/cpp-3.4_3.4.6-6ubuntu3_i386.deb
-dpkg -i cpp-3.4_3.4.6-6ubuntu3_i386.deb 
-wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/gcc-3.4_3.4.6-6ubuntu3_i386.deb
-dpkg -i gcc-3.4_3.4.6-6ubuntu3_i386.deb
-wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/libstdc++6-dev_3.4.6-6ubuntu3_i386.deb
-dpkg --force-depends -i libstdc++6-dev_3.4.6-6ubuntu3_i386.deb
-wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/g++-3.4_3.4.6-6ubuntu3_i386.deb
-dpkg -i g++-3.4_3.4.6-6ubuntu3_i386.deb
+wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/gcc-3.4-base_3.4.6-6ubuntu3_i386.deb &&
+dpkg -i gcc-3.4-base_3.4.6-6ubuntu3_i386.deb &&
+wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/cpp-3.4_3.4.6-6ubuntu3_i386.deb &&
+dpkg -i cpp-3.4_3.4.6-6ubuntu3_i386.deb &&
+wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/gcc-3.4_3.4.6-6ubuntu3_i386.deb &&
+dpkg -i gcc-3.4_3.4.6-6ubuntu3_i386.deb &&
+wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/libstdc++6-dev_3.4.6-6ubuntu3_i386.deb &&
+dpkg --force-depends -i libstdc++6-dev_3.4.6-6ubuntu3_i386.deb && 
+wget http://old-releases.ubuntu.com/ubuntu/pool/universe/g/gcc-3.4/g++-3.4_3.4.6-6ubuntu3_i386.deb &&
+dpkg -i g++-3.4_3.4.6-6ubuntu3_i386.deb 
